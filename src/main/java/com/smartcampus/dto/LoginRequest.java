65d5d28f12790f63915ff833523d2e4f2e03e4a5
Mehara Rothila Ranawaka <rothilamehara22@gmail.com>
2025-10-16 21:59:06 +0530

    refactor: Update LoginRequest to support username or email

--- src/main/java/com/smartcampus/dto/LoginRequest.java
+++ src/main/java/com/smartcampus/dto/LoginRequest.java
@@ -4,21 +4,25 @@ import jakarta.validation.constraints.NotBlank;
 
 public class LoginRequest {
     @NotBlank
-    private String username;
-    
+    private String usernameOrEmail;
+
     @NotBlank
     private String password;
-    
+
     public LoginRequest() {}
-    
-    public LoginRequest(String username, String password) {
-        this.username = username;
+
+    public LoginRequest(String usernameOrEmail, String password) {
+        this.usernameOrEmail = usernameOrEmail;
         this.password = password;
     }
-    
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-    
+
+    public String getUsernameOrEmail() { return usernameOrEmail; }
+    public void setUsernameOrEmail(String usernameOrEmail) { this.usernameOrEmail = usernameOrEmail; }
+
+    // Keep legacy getter/setter for backward compatibility
+    public String getUsername() { return usernameOrEmail; }
+    public void setUsername(String username) { this.usernameOrEmail = username; }
+
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
 }
