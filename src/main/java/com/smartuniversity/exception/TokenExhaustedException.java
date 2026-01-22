22b0cb05ad663227b8ba2c78f836567ef7195fa4
Mehara Rothila Ranawaka <rothilamehara22@gmail.com>
2026-01-22 22:53:12 +0530

    feat(ai): add TokenExhaustedException for daily limit handling

---
+++ src/main/java/com/smartuniversity/exception/TokenExhaustedException.java
@@ -0,0 +1,7 @@
+package com.smartuniversity.exception;
+
+public class TokenExhaustedException extends RuntimeException {
+    public TokenExhaustedException(String message) {
+        super(message);
+    }
+}
